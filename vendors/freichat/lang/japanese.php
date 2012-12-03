551b5ff0d926483577e39118cf5c9cba2666ee92
yumokin2 <yumokin2@yumoto-no-MacBook-Air.local>
2012-12-04 06:26:33 +0900

    japanese langage
    I created japanese langage.

---
+++ vendors/freichat/lang/japanese.php
@@ -0,0 +1,70 @@
+<?php
+//FreiChatX - Japanese language
+//Japanese is the primary language
+//cb->chatbox
+//g->guest
+//Do not use space but nbsp;
+//If you have copied this file and you are making your own
+//language then please write in the format similar to
+//english_informal.php
+
+
+//NOTE: &nbsp; is euivalent to a space 
+
+    $frei_trans['cb_head']           =          '参加ユーザ';
+    $frei_trans['g_prefix']          =          'ゲスト-';
+    $frei_trans['pwdby']             =          'Powered by';
+    $frei_trans['noline']            =          '誰もチャットルームにいません';
+    $frei_trans['noperms']           =          'チャットルームへログインして下さい!';
+    $frei_trans['on_offline']        =          'しばらくお待ち下さい';
+    $frei_trans['go_online']         =          'オンラインへ';
+    $frei_trans['go_offline']        =          'オフラインへ';
+    $frei_trans['go_invisible']      =          '中断へ';
+    $frei_trans['go_busy']           =          'ビジーへ';
+    $frei_trans['newmesg']           =          '新しいメッセージ! From';
+    $frei_trans['restore_drag_pos']  =          'チャットウィンドウの位置を戻して下さい';
+    $frei_trans['status_txt']        =          '状態の変更';
+    $frei_trans['opt_txt']           =          '追加のオプション';
+    $frei_trans['onOfflinemesg']     =          'チャットをクリックして下さい!';
+
+    $frei_trans['plugin_transdisable']  =       '中断';
+    $frei_trans['plugin_trans_orig']    =       'オリジナルメッセージ';
+
+    $frei_trans['titles_translate']     =       'あなたの言語へ翻訳して下さい';
+    $frei_trans['titles_clrcht']     =       'チャット履歴をクリアして下さい';
+    $frei_trans['titles_upload']        =       'ファイルをアップロードして下さい';
+    $frei_trans['titles_smiley']        =       'スマイリーを挿入して下さい';
+    $frei_trans['titles_save']        =       'チャット履歴を保存して下さい';
+    $frei_trans["titles_mail"] 		=       '挨拶としてメールを送って下さい';
+
+    $frei_trans['status_online']        =       'オンライン';
+    $frei_trans['status_busy']          =       'ビジー';
+    $frei_trans['status_invisible']     =       '中断';
+    $frei_trans['status_offline']       =       'オフライン';
+    $frei_trans['default_status']       =       'デフォルト';
+
+    $frei_trans['status_txt_online']        =       'オンライン状態';
+    $frei_trans['status_txt_busy']          =       'ビジー状態';
+    $frei_trans['status_txt_invisible']     =       '中断状態';
+    $frei_trans['status_txt_offline']       =       'オフライン状態';
+
+    
+    
+    
+    $frei_trans['chatHistoryNotFound']  =       'チャット履歴が見つかりません!';
+    $frei_trans['chatHistoryDeleted']   =       'チャット履歴がクリアされました';
+
+    $frei_trans['set_custom_mesg']   =       'カスタムメッセージを設定して下さい:';
+    
+    $frei_trans['chat_room_title'] = 'チャットルーム';
+    
+    $frei_trans['chat_message_me'] = '名無しさん';
+    
+    $frei_trans['chat_status'] = 'ユーザはオフラインです。あなたが送ったメッセージはユーザがオンラインになったとき届けられます';
+    
+    $frei_trans['rtl'] = 'Swicth rtl text';
+    
+    $frei_trans['profilelink'] = 'プロフィールを見る';
+    
+         return 1;
+?>
